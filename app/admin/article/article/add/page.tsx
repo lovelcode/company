fb830634d06d61be4774bf0c8f218d66004cefc5
spdfalcon <spd.mohammadreza@gmail.com>
2024-09-05 22:25:08 -0700

    end add article

--- app/admin/article/article/add/page.tsx
+++ app/admin/article/article/add/page.tsx
@@ -1,33 +1,59 @@
-'use client'
-import { CKEditor } from "ckeditor4-react";
+"use client";
+
+import EditorComponent from "@/app/_components/modules/Editoe/Editor";
 
 const page = () => {
   return (
-    <div className="container flex flex-col gap-3">
+    <div className="container flex flex-col gap-20">
       <div className="flex flex-col gap-2 md:flex-row md:justify-between">
         <label htmlFor="">نویسنده:</label>
-        <select className="text-black w-52" name="" id="">
+        <select className="text-black w-52 " name="" id="">
           <option value="">mohammad</option>
           <option value="">ali</option>
         </select>
       </div>
-      <div className="flex flex-col gap-2 md:flex-row md:justify-between">
+
+      <div className="flex flex-col gap-2 md:flex-row md:justify-between md:items-center">
         <label htmlFor="">Meta title (SEO):</label>
-        <textarea className="rounded-md" rows={5} name="" id=""></textarea>
+        <textarea
+          className="rounded-md w-full max-w-[600px] text-black"
+          rows={5}
+          name=""
+          id=""
+        ></textarea>
       </div>
-      <div className="flex flex-col gap-2 md:flex-row md:justify-between">
+
+      <div className="flex flex-col gap-2 md:flex-row md:justify-between md:items-center">
         <label htmlFor="">Meta description (SEO):</label>
-        <textarea className="rounded-md" rows={5} name="" id=""></textarea>
+        <textarea
+          className="rounded-md w-full max-w-[600px] text-black"
+          rows={5}
+          name=""
+          id=""
+        ></textarea>
       </div>
-      <div className="flex flex-col gap-2 md:flex-row md:justify-between">
-        <label htmlFor="">نویسنده:</label>
-        <div className="flex gap-3">
-          <label htmlFor="">تاریخ:</label>
-          <input type="text" className="text-black w-52" name="" id=""></input>
-        </div>
-        <div className="flex gap-3">
-          <label htmlFor="">زمان:</label>
-          <input type="text" className="text-black w-52" name="" id=""></input>
+
+      <div className="flex justify-between items-center ">
+        <label htmlFor="">تاریخ ایجاد:</label>
+        <div className="flex flex-col gap-3">
+          <div className="flex gap-3">
+            <label htmlFor="">تاریخ:</label>
+            <input
+              type="text"
+              className="text-black w-52"
+              name=""
+              id=""
+            ></input>
+          </div>
+          <div className="flex gap-3">
+            <label htmlFor="">زمان:</label>
+            <input
+              type="text"
+              className="text-black w-52"
+              name=""
+              id=""
+            ></input>
+          </div>
         </div>
       </div>
 
@@ -49,8 +75,62 @@ const page = () => {
         <input type="text" className="text-black w-52" name="" id=""></input>
       </div>
 
-      
-      <CKEditor></CKEditor>
+      <div className="text-black">
+        <h1>صفحه ویرایشگر</h1>
+        <EditorComponent></EditorComponent>
+      </div>
+
+      <div className="flex flex-col gap-2 md:flex-row md:justify-between">
+        <label className="font-bold" htmlFor="">
+          تصویر شاخص:
+        </label>
+        <input type="file" name="" id="" />
+      </div>
+
+      <div className="">
+        <label className="" htmlFor="">
+          در سایت منتشر شود
+        </label>
+        <input type="checkbox" name="" id="" />
+        <br />
+        <p className="text-xs ">
+          غیرفعال بودن این فیلد به معنی پیش نویس بودن مقاله و عدم انتشار آن در
+          سایت می‌باشد.
+        </p>
+      </div>
+
+      <div className="flex flex-col gap-2 md:flex-row md:justify-between">
+        <label className="font-bold" htmlFor="">
+          بازدیدها:
+        </label>
+        <input
+          className="text-black w-52 text-xl"
+          type="number"
+          name=""
+          id=""
+        />
+      </div>
+      <div className="flex flex-col gap-2 md:flex-row md:justify-between">
+        <label className="font-bold" htmlFor="">
+          بازدیدها:
+        </label>
+        <input
+          className="text-black w-52 text-xl"
+          type="number"
+          name=""
+          id=""
+        />
+      </div>
+
+      <div className="flex flex-col gap-2 md:flex-row md:justify-between">
+        <h3 className="mb-4 font-semibold text-gray-900 dark:text-white">
+          هشتگ ها:
+        </h3>
+        <select multiple className="text-black w-52" name="" id="">
+          <option value="">mohammad</option>
+          <option value="">ali</option>
+        </select>
+      </div>
     </div>
   );
 };
